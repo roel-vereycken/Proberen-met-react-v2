bf954164584981c73adeeb1189bca7ba5c7713af
roel-vereycken <vereyckenroel@gmail.com>
2021-04-10 14:17:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
+  Input,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Flex,
+  Container,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 
-function App() {
+const App = () => {
+  const [voornaam, setVoorNaam] = useState('');
+  const [naam, setNaam] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleFormSubmit = event => {
+    event.preventDefault();
+
+    fetch('http://127.0.0.1:8000/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        naam: naam,
+        voornaam: voornaam,
+        photo: '/',
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('gelukt');
+        console.log(data);
+      })
+      .catch(error => {
+        console.log('mislukt');
+        console.log(error);
+      })
+      .finally(() => {
+        setNaam('');
+        setVoorNaam('');
+        setPassword('');
+        setEmail('');
+      });
+  };
+
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+    <>
+      <Container maxW="container.md" centerContent>
+        <h1 text-allign="center">Registreerformulier</h1>
+        <FormControl>
+          <form onSubmit={handleFormSubmit}>
+            <Flex align="center" justify="center" flexDirection="column">
+              <Input
+                mb="10"
+                type="text"
+                name="voornaam"
+                value={voornaam}
+                placeholder="Jouw voornaam"
+                onChange={e => setVoorNaam(e.target.value)}
+              />
+              <Input
+                mb="10"
+                type="text"
+                name="naam"
+                value={naam}
+                placeholder="Jouw naam"
+                onChange={e => setNaam(e.target.value)}
+              />
+              <Input
+                mb="10"
+                type="password"
+                name="password"
+                value={password}
+                placeholder="Wachtwoord"
+                onChange={e => setPassword(e.target.value)}
+              />
+              <Input
+                mb="10"
+                type="email"
+                name="email"
+                value={email}
+                placeholder="Email"
+                onChange={e => setEmail(e.target.value)}
+              />
+              <Input type="submit" value="Registreer" />
+            </Flex>
+          </form>
+        </FormControl>
+      </Container>
+    </>
   );
-}
+};
 
 export default App;
