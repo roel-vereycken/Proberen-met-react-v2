9a232e505e8dee8b8624492f7a9f90a889e8071d
Petervrancken <peter.vrancken88@gmail.com>
2021-04-14 16:12:38 +0200

    Working on styling

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,10 +1,12 @@
 import React, {useState} from 'react'
-import Navbar from "./Navbar"
 import {
     Input,
     FormControl,
     Flex,
-    Text
+    Center,
+    Text,
+    Box,
+    Image
   } from '@chakra-ui/react';
 
 
@@ -12,30 +14,77 @@ function LoginForm() {
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
 
+
+    // VRAAG: IMAGE NEEMT GEEN PNG NOG JPEG AAN, ENKEL WEB URL'S
     return (
         <>
-        <Navbar />
-        <Text  fontWeight="extrabold" fontSize={32} textAlign={[ 'left', 'center' ]}>Login</Text>
+        <Image  src='../afbeeldingen/thedisplacementapplogo.png' w={10} h={10} />
+
+        <Center>
+        <Box id="logobackgroundtest">
+        </Box>
+        </Center>
+        
         <FormControl>
           <form>
             <Flex align="center" justify="center" flexDirection="column">
-              <Input
-                mb="10"
-                type="password"
-                name="password"
-                value={password}
-                placeholder="Wachtwoord"
-                onChange={e => setPassword(e.target.value)}
-              />
-              <Input
-                mb="10"
-                type="email"
-                name="email"
-                value={email}
-                placeholder="Email"
-                onChange={e => setEmail(e.target.value)}
-              />
-              <Input type="submit" value="Registreer" />
+              <Box width="20%" minWidth="300px">
+                <Text fontSize="16px" mb="3" color="#3cf0f0">
+                  Email:
+                </Text>
+                <Input
+                  color="black"
+                  outline="none"
+                  bg="white"
+                  borderRadius="5"
+                  height="25"
+                  width="100%"
+                  border="none"
+                  type="email"
+                  name="email"
+                  value={email}
+                  placeholder="Email"
+                  onChange={e => setEmail(e.target.value)}
+                />
+                <Text fontSize="16px" mb="3" color="#3cf0f0">
+                  Wachtwoord:
+                </Text>
+                  <Input
+                    color="black"
+                    outline="none"
+                    bg="white"
+                    borderRadius="5"
+                    border="none"
+                    height="25"
+                    width="100%"
+                    type="password"
+                    name="password"
+                    value={password}
+                    placeholder="Wachtwoord"
+                    onChange={e => setPassword(e.target.value)}
+                  />
+                  <Box mt="15px" bgColor="none">
+                    <Flex align="center">
+                    <Input 
+                      id="buttonHover"
+                      type="submit"
+                      border="none"
+                      boxShadow="none"
+                      outline="none"
+                      height="30px"
+                      width="130px"
+                      value="Verzenden" 
+                      color="white"
+                      outline="none"
+                      bg="#00326f"
+                      borderRadius="5"
+                    />
+                    <Text fontSize="12px" color="#00326f" my="0px" ml="5px">
+                      Nog geen lid? Maak <a href="#">hier</a> een nieuw account aan!
+                    </Text>
+                    </Flex>
+                  </Box>
+              </Box>
             </Flex>
           </form>
         </FormControl>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { WarningIcon, CheckCircleIcon } from '@chakra-ui/icons'
+import Logo from '../afbeeldingen/thedisplacementapplogo.png';
+import Dummypic from '../afbeeldingen/Dummypic.jpg';
 import {
     Menu,
     MenuButton,
@@ -7,8 +9,9 @@ import {
     MenuItem,
     Box,
     Flex,
-    Icon,
-    IconButton
+    Text,
+    IconButton,
+    Image
   } from "@chakra-ui/react"
 
 
@@ -23,19 +26,19 @@ function Navbar() {
         <Box py={20} backgroundColor="yellow">
             <Flex alignItems={"center"}>
             <Box mr={"auto"}>
-                <a href="#" ><WarningIcon w={100} h={100} color="red" /> </a>
+                <a href="#" ><Image  src={Logo} w={100} h={70} /> </a>
             </Box>
-            <Box mr={10}><a href="#"><p>Logout</p></a></Box>
+            <Box mr={10}><a href="#"><Text fontSize="10px ">Logout</Text></a></Box>
             
 
             <Menu>
-                <MenuButton as={IconButton} borderRadius={50} variant={"link"} cursor={'pointer'}>
-                <CheckCircleIcon  w={50} h={50} color="green"/>
+                <MenuButton as={IconButton} borderRadius={50} width="48px" height="48px" variant={"link"} cursor={'pointer'}>
+                <Image borderRadius={50}  src={Dummypic} w={50} h={50} />
                 </MenuButton>
                     <MenuList>
-                        <MenuItem>Profiel</MenuItem>
-                        <MenuItem>Verplaatsing</MenuItem>
-                        <MenuItem>Overzicht</MenuItem>
+                        <MenuItem color="white">Profiel</MenuItem>
+                        <MenuItem color="white">Verplaatsing</MenuItem>
+                        <MenuItem color="white">Overzicht</MenuItem>
                     </MenuList>
             </Menu>
             </Flex>
