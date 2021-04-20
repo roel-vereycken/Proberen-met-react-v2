5299c3302f03208b696fc8d90c24801989360035
roel-vereycken <vereyckenroel@gmail.com>
2021-04-20 02:57:25 +0200

    Fix prijzen

--- src/App.js
+++ src/App.js
@@ -6,25 +6,27 @@ import Navbar from './components/Navbar';
 import LoginForm from './components/LoginForm';
 import ProfielPagina from './components/ProfielPagina';
 import Verplaatsingen from './components/Verplaatsingen';
-import Styles from './style/style.scss'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Vervoersmiddel from './components/Vervoersmiddel';
+import Styles from './style/style.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <Router>
-    <>
-    <Switch>
-      <Route exact path="/login" component={LoginForm}/>
-      <Route path="/registratie" component={RegisterForm}/>
-      <div>
-      <Navbar/>
-      <Route path="/verplaatsing" component={Verplaatsingen}/>
-      <Route path="/profiel" component={ProfielPagina}/>
-      <Route path="/wachtwoord" component={PasswordForm}/>
-      <Route path="/overzicht" component={OverzichtGrid}/>
-      </div>
-    </Switch>
-    </>
+      <>
+        <Switch>
+          <Route exact path="/login" component={LoginForm} />
+          <Route path="/registratie" component={RegisterForm} />
+          <div>
+            <Navbar />
+            <Route path="/verplaatsing" component={Verplaatsingen} />
+            <Route path="/profiel" component={ProfielPagina} />
+            <Route path="/wachtwoord" component={PasswordForm} />
+            <Route path="/overzicht" component={OverzichtGrid} />
+            <Route path="/vervoersmiddel" component={Vervoersmiddel} />
+          </div>
+        </Switch>
+      </>
     </Router>
   );
 };
